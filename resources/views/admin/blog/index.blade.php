69e3a967995473d80fec4c9bdf314b35086b7a6f
Picors <husnipicors10@gmail.com>
2024-09-19 14:45:47 +0700

    nyoba

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -10,7 +10,7 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard >></a></li>
                             <li class="breadcrumb-item active">Data Blog</li>
                         </ol>
                     </div>
